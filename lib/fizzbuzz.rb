6ab3f77bbb6973578e80db35167b7e02f27ac2ef
Abigail <ajmakovsky@gmail.com>
2023-01-10 17:21:52 +0000

    Update code to run using the Fizzbuzz Integer method and return a list of given Fizzbuzz numbers

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,8 +5,40 @@ class Integer
         elsif self % 3 == 0
           'Fizz'
         elsif self % 5 == 0
-          'Buzz'    
+          'Buzz'
+        else
+          self        
         end  
     end    
 end
+# 1 --> 1
+# 2 --> 2
+# 3 --> fizz
+# 4 --> 4
+# 5 --> buzz
+# 6 --> fizz
+# 7 --> 7
+# 8 --> 8
+# 9 --> fizz
+# 10 --> buzz
 
+# Integer.fizzbuzz => 'FizzBuzz' / 'Fizz' / 'Buzz' / Integer
+#
+
+def fizzbuzzlist(start, last)
+    output = ''
+    (start..last).each do |number|
+    # number.fizzbuzz.to_s => if 3 = Fizz ; 4 = 4
+    output += number.to_s + " --> " + number.fizzbuzz.to_s + "\n"
+    # output += "#{number} --> #{number.fizzbuzz}\n"
+    #   if number == 3
+    #     output += "3 => Fizz\n" # 1st => "3 => Fizz\n"
+    #   elsif number == 4
+    #     output += "4 => 4\n" # 2nd => "3 => Fizz\n4 => 4\n"
+    #   end
+    end
+    output[-1] = ''
+    return output
+end
+
+puts fizzbuzzlist(1,20)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,30 @@
 # In all other cases, the program simply returns the given number.
 # Eg: 3.fizzbuzz => 'Fizz'
 
+# Write a test for 'fizzbuzzlist' which output a range of fizzbuzz number.
+# Method => fizzbuzzlist(start, last)
+# Output:
+# 1 --> 1
+# 2 --> 2
+# 3 --> fizz
+# 4 --> 4
+# 5 --> buzz
+# 6 --> fizz
+# 7 --> 7
+# 8 --> 8
+# 9 --> fizz
+# 10 --> buzz
+# 11 --> 11
+# 12 --> fizz
+# 13 --> 13
+# 14 --> 14
+# 15 --> fizzbuzz
+# 16 --> 16
+# 17 --> 17
+# 18 --> fizz
+# 19 --> 19
+# 20 --> buzz
+
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
@@ -20,4 +44,18 @@ describe 'fizzbuzz' do
   it 'returns "Fizzbuzz" when passed a number that is a multiple of both 3 and 5' do
     expect(15.fizzbuzz).to eq 'FizzBuzz'
   end 
+  
+  it 'returns integer 1 when passed integer 1' do 
+    expect(1.fizzbuzz).to eq 1 
+  end
 end
+
+describe 'fizzbuzzlist' do 
+  it 'returns "3 --> Fizz" when passed 3' do 
+    expect(fizzbuzzlist(3,3)).to eq("3 --> Fizz")
+  end 
+
+  it 'returns "3 --> Fizz" and "4 --> 4" when passed 3, 4' do
+    expect(fizzbuzzlist(3,4)).to eq("3 --> Fizz\n4 --> 4")
+  end
+end 
